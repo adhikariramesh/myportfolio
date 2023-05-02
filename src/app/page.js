89e5d0c3d4bff68e2dfd0e867e3a982ff3dedc4b
Ramesh Prasad Adhikari <107765170+adhikariramesh@users.noreply.github.com>
2023-05-02 13:02:21 +0545

    removing about from import section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import HeroSection from './components/heroSection'
-import About from './components/About'
 import ContactSection from './components/ContactSection'
 const page = () => {
   return (
     <>
       <HeroSection />
-      {/* <About/> */}
       <ContactSection/>
     </>
   )
